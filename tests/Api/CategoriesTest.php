465821a77360cfdc67f927b3ba13e7ea48bc8bb5
vivekssharma07 <35025797+vivekssharma07@users.noreply.github.com>
2018-05-17 18:45:09 +0530

    Update CategoriesTest.php

--- tests/Api/CategoriesTest.php
+++ tests/Api/CategoriesTest.php
@@ -1,15 +1,7 @@
 <?php
-/**
- * @package     Mautic
- * @copyright   2014 Mautic, NP. All rights reserved.
- * @author      Mautic
- * @link        http://mautic.org
- * @license     MIT http://opensource.org/licenses/MIT
- */
+namespace Leadsengage\Tests\Api;
 
-namespace Mautic\Tests\Api;
-
-class CategoriesTest extends MauticApiTestCase
+class CategoriesTest extends LeadsengageApiTestCase
 {
     public function setUp() {
         $this->api = $this->getContext('categories');
