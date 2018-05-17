f953f17a70660199dcc18d2b646355b25f31175b
vivekssharma07 <35025797+vivekssharma07@users.noreply.github.com>
2018-05-17 18:45:32 +0530

    Update CompaniesTest.php

--- tests/Api/CompaniesTest.php
+++ tests/Api/CompaniesTest.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * @package     Mautic
- * @copyright   2014 Mautic, NP. All rights reserved.
- * @author      Mautic
- * @link        http://mautic.org
- * @license     MIT http://opensource.org/licenses/MIT
- */
-
-namespace Mautic\Tests\Api;
+namespace Leadsengage\Tests\Api;
 
 class CompaniesTest extends AbstractCustomFieldsTest
 {
